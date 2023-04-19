0ec4a18b51009014c94e8d6381c764c8897bef71
Andrew Zaiko <andrewzaiko@gmail.com>
2023-04-19 18:45:08 +0300

    Настроил фильтр модели 'Company' по городу, поиск по названию и очистку долга перед поставщиком

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company
+
+
+# Регистрируем модели с собственными панелями администратора
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    """Модель участника торговой сети"""
+    list_display = ['title', 'role']
+    list_filter = ['town']
+    search_fields = ['title']
+    search_help_text = 'Поиск по названию участника торговой сети'
+    actions = ['clear_debt']
+
+    @admin.action(description='Очистить задолженность перед поставщиком')
+    def clear_debt(self, request, queryset):
+        queryset.update(debt=0)
